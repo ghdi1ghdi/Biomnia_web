56f7c6a31ebad4ed8bf32ac4cdb3c3c2cc35f615
Lee Jeong Ho <73730434+ghdi1ghdi@users.noreply.github.com>
2023-07-21 04:01:20 +0900

    Update featurecomponent.jsx

--- src/views/components/sections/featurecomponent.jsx
+++ src/views/components/sections/featurecomponent.jsx
@@ -18,6 +18,7 @@ const FeatureComponent = () => {
               <h1 className="title font-bold">BIOMNIA?</h1>
               <h6 className="subtitle">
                 BIOMNIA는 맞춤형 마이크로바이옴(microbiome) 데이터를 활용한
+                <br></br>
                 임산부 및 영유아 부모를 위한 식자재 구독 서비스입니다.
               </h6>
             </Col>
